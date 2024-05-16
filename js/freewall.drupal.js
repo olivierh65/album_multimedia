4aee035314b8c3c2152555ecef7032a2a9edf988
olivierh65 <ol65@sfr.fr>
2024-05-16 22:17:37 +0200

    add wookmark layout. Wait image loading before initilize freewall

--- js/freewall.drupal.js
+++ js/freewall.drupal.js
@@ -1,31 +1,60 @@
 (function ($, Drupal, drupalSettings) {
-  // $(document).ready(function (e, settings) {
-    var wall;
-    Drupal.behaviors.flexwall = {
-      attach: function (context, settings) {
-    console.log("Load freewall");
-    wall = new Freewall("#freewall");
-    wall.reset({
-      selector: ".lightgallery-content-1",
-      animate: true,
-      cellW: 300,
-      cellH: 'auto',
-      cacheSize: false,
-      keepOrder: true,
-      onResize: function () {
-        wall.fitWidth();
-      },
-    });
-  
-    wall.container.find('.freewall-brick img').on('load',function() {
+  $(document).foundation();
+
+  var images_loaded = 0;
+  var total_images = $(".lightgallery-image-1").length;
+  var percent_slice = 100 / total_images;
+  var current_percent = 0;
+
+  $(".lightgallery-image-1")
+    .one("load", function () {
+      images_loaded++;
+      current_percent += percent_slice;
       // move info in the same div than the div contening image
-      jQuery('.freewall-brick').each(function() {
-        jQuery(this).find('.freewall-info').detach().appendTo(jQuery(this).find('.lightgallery-content-1'))
+      jQuery(".freewall-brick").each(function () {
+        jQuery(this)
+          .find(".freewall-info")
+          .detach()
+          .appendTo(jQuery(this).find(".lightgallery-content-1"));
       });
-      wall.fitWidth();
+      if (images_loaded == total_images) {
+        var wall;
+        Drupal.behaviors.flexwall = {
+          attach: function (context, settings) {
+            console.log("Load freewall");
+            wall = new Freewall("#freewall");
+            wall.reset({
+              selector: ".lightgallery-content-1",
+              animate: true,
+              cellW: 300,
+              cellH: "auto",
+              cacheSize: false,
+              keepOrder: true,
+              onResize: function () {
+                wall.fitWidth();
+              },
+            });
+          },
+        };
+      }
+      // $(".loader-wrapper").hide();
+    })
+    .each(function () {
+      if (this.complete) {
+        // $(this).load();
+        $(this).trigger("load");
+      }
     });
-    
+  // $(document).ready(function (e, settings) {
 
-  }};
-
+  wall.container.find(".freewall-brick img").on("load", function () {
+    // move info in the same div than the div contening image
+    jQuery(".freewall-brick").each(function () {
+      jQuery(this)
+        .find(".freewall-info")
+        .detach()
+        .appendTo(jQuery(this).find(".lightgallery-content-1"));
+    });
+    wall.fitWidth();
+  });
 })(jQuery, Drupal, drupalSettings);
