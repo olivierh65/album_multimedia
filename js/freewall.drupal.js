628d9d9d3e814ba40ea661845f563c6ae3302196
olivierh65 <ol65@sfr.fr>
2024-05-16 23:25:19 +0200

    correction .drupal.js

--- js/freewall.drupal.js
+++ js/freewall.drupal.js
@@ -1,53 +1,12 @@
 (function ($, Drupal, drupalSettings) {
-  $(document).foundation();
+  $(document).ready(function () {
+    $(document).foundation();
 
-  var images_loaded = 0;
-  var total_images = $(".lightgallery-image-1").length;
-  var percent_slice = 100 / total_images;
-  var current_percent = 0;
+    var images_loaded = 0;
+    var total_images = $(".lightgallery-image-1").length;
+    var percent_slice = 100 / total_images;
+    var current_percent = 0;
 
-  $(".lightgallery-image-1")
-    .one("load", function () {
-      images_loaded++;
-      current_percent += percent_slice;
-      // move info in the same div than the div contening image
-      jQuery(".freewall-brick").each(function () {
-        jQuery(this)
-          .find(".freewall-info")
-          .detach()
-          .appendTo(jQuery(this).find(".lightgallery-content-1"));
-      });
-      if (images_loaded == total_images) {
-        var wall;
-        Drupal.behaviors.flexwall = {
-          attach: function (context, settings) {
-            console.log("Load freewall");
-            wall = new Freewall("#freewall");
-            wall.reset({
-              selector: ".lightgallery-content-1",
-              animate: true,
-              cellW: 300,
-              cellH: "auto",
-              cacheSize: false,
-              keepOrder: true,
-              onResize: function () {
-                wall.fitWidth();
-              },
-            });
-          },
-        };
-      }
-      // $(".loader-wrapper").hide();
-    })
-    .each(function () {
-      if (this.complete) {
-        // $(this).load();
-        $(this).trigger("load");
-      }
-    });
-  // $(document).ready(function (e, settings) {
-
-  wall.container.find(".freewall-brick img").on("load", function () {
     // move info in the same div than the div contening image
     jQuery(".freewall-brick").each(function () {
       jQuery(this)
@@ -55,6 +14,40 @@
         .detach()
         .appendTo(jQuery(this).find(".lightgallery-content-1"));
     });
-    wall.fitWidth();
+
+    $(".lightgallery-image-1")
+      .one("load", function () {
+        images_loaded++;
+        current_percent += percent_slice;
+
+        if (images_loaded == total_images) {
+          console.log("Load freewall");
+          var wall;
+          wall = new Freewall("#freewall");
+          wall.reset({
+            selector: ".lightgallery-content-1",
+            animate: true,
+            cellW: 300,
+            cellH: "auto",
+            cacheSize: false,
+            keepOrder: true,
+            onResize: function () {
+              wall.fitWidth();
+            },
+          });
+          wall.container.find(".freewall-brick img").on("load", function () {
+            wall.fitWidth();
+          });
+        }
+        // $(".loader-wrapper").hide();
+      })
+      .each(function () {
+        if (this.complete) {
+          // $(this).load();
+          $(this).trigger("load");
+        }
+      });
+
+    // $(document).ready(function (e, settings) {
   });
 })(jQuery, Drupal, drupalSettings);
--- js/wookmark.drupal.js
+++ js/wookmark.drupal.js
@@ -1,40 +1,42 @@
 (function ($, Drupal, drupalSettings) {
-  $(document).foundation();
+  $(document).ready(function () {
+    $(document).foundation();
 
-  var images_loaded = 0;
-  var total_images = $(".lightgallery-image-1").length;
-  var percent_slice = 100 / total_images;
-  var current_percent = 0;
+    var images_loaded = 0;
+    var total_images = $(".lightgallery-image-1").length;
+    var percent_slice = 100 / total_images;
+    var current_percent = 0;
 
-  $(".lightgallery-image-1")
-    .one("load", function () {
-      images_loaded++;
-      current_percent += percent_slice;
-      // move info in the same div than the div contening image
-      jQuery(".freewall-brick").each(function () {
-        jQuery(this)
-          .find(".freewall-info")
-          .detach()
-          .appendTo(jQuery(this).find(".lightgallery-content-1"));
-      });
-      if (images_loaded == total_images) {
-        var w = $("#freewall").wookmark({
-          align: "center",
-          autoResize: true,
-          ignoreInactiveItems: true,
-          itemWidth: 250,
-          fillEmptySpace: false,
-          flexibleWidth: "40%",
-          offset: 2,
-          resizeDelay: 50,
+    $(".lightgallery-image-1")
+      .one("load", function () {
+        images_loaded++;
+        current_percent += percent_slice;
+        // move info in the same div than the div contening image
+        jQuery(".freewall-brick").each(function () {
+          jQuery(this)
+            .find(".freewall-info")
+            .detach()
+            .appendTo(jQuery(this).find(".lightgallery-content-1"));
         });
-        // $(".loader-wrapper").hide();
-      }
-    })
-    .each(function () {
-      if (this.complete) {
-        // $(this).load();
-        $(this).trigger("load");
-      }
-    });
+        if (images_loaded == total_images) {
+          var w = $("#freewall").wookmark({
+            align: "center",
+            autoResize: true,
+            ignoreInactiveItems: true,
+            itemWidth: 250,
+            fillEmptySpace: false,
+            flexibleWidth: "40%",
+            offset: 2,
+            resizeDelay: 50,
+          });
+          // $(".loader-wrapper").hide();
+        }
+      })
+      .each(function () {
+        if (this.complete) {
+          // $(this).load();
+          $(this).trigger("load");
+        }
+      });
+  });
 })(jQuery, Drupal, drupalSettings);
